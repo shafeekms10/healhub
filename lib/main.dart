4732fb792d63bbeb26327f108111cd928ed375eb
shafeekms10 <143930057+shafeekms10@users.noreply.github.com>
2024-02-14 01:17:01 +0530

    Fire Base Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'Screens/signin_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: SignIn(),
