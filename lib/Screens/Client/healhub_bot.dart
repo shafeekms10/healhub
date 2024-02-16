210098ff4e7e27a58f6c52c7c73cf2a9764218c8
shafeekms10 <143930057+shafeekms10@users.noreply.github.com>
2024-02-16 06:51:13 +0530

    bot

--- lib/Screens/Client/healhub_bot.dart
+++ lib/Screens/Client/healhub_bot.dart
@@ -9,6 +9,7 @@ class Chatbot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'HealHubBot',
       theme: ThemeData(
         brightness: Brightness.light
